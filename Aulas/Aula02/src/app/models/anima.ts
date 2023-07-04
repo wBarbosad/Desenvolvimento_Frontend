7f5d778976fab5ced1841f07657d0cea557b0941
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-07-03 22:01:13 -0300

    Aula 02

---
+++ Aulas/Aula02/src/app/models/anima.ts
@@ -0,0 +1,16 @@
+export class Animal{
+  _id!: string;
+  nome!: string;
+  raca!: string;
+  peso!: number;
+
+  constructor(_id: string, nome: string, raca: string, peso: number){
+    this._id = _id;
+    this.nome = nome;
+    this.raca = raca;
+    this.peso = peso;
+  }
+  
+}
+
+
